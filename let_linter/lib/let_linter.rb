a59cd65f70b7f153f5fc35660df089570b75fb3f
s.sagara <>
2025-03-18 00:37:14 +0900

    gemプロジェクト作成

---
+++ let_linter/lib/let_linter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "let_linter/version"
+
+module LetLinter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ let_linter/lib/let_linter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LetLinter
+  VERSION = "0.1.0"
+end
